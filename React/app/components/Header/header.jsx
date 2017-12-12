044731e0e764b8f0d18e0af01edd09f5108ba740
lantic <gytlantic@outlook.com>
2017-12-12 23:48:39 +0800

    Init

---
+++ React/app/components/Header/header.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './header.scss';
+
+class Header extends Component {
+	render() {
+		return (
+			<p className="hl">Hello World!</p>
+		)
+	}
+}
+
+export default Header
---
+++ React/app/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Header from './components/Header/header.jsx';
+
+ReactDom.render(
+	<Header />,
+	document.querySelector("#root")
+);
