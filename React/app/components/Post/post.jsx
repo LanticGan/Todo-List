c7175132cbffed5f07904bd54320f125432ed2f2
lantic <gytlantic@outlook.com>
2017-12-14 10:59:12 +0800

    Update

--- React/app/components/Post/post.jsx
+++ React/app/components/Post/post.jsx
@@ -2,30 +2,47 @@ import React, {Component} from 'react';
 import './post.scss';
 
 class Post extends Component {
+
+	constructor(props) {
+		super(props);
+		this.cancelPost=this.cancelPost.bind(this);
+	}
+
+	cancelPost() {
+		this.props.handlePostShow(false)
+	}
+
 	render() {
-		return (
-			<div className="post-container">
-				<div className="post-title">
-					<label>添加任务</label>
-					<div className="cancel-icon">
-						<img src="assets/add.png" />
+		let show = this.props.show;
+		if (show) {
+			return (
+				<div>
+					<div className="cover"></div>
+					<div className="post-container">
+						<div className="post-title">
+							<label>添加任务</label>
+							<div className="cancel-icon" onClick={this.cancelPost}>
+								<img src="assets/cancel.png" />
+							</div>
+						</div>
+						<div className="post-input">
+							<input type="text" placeholder="请输入TodoItem"/>
+						</div>
+						<div className="post-confirm">
+							添加任务
+						</div>
+						<div className="post-filter">
+							<p>设置优先级</p>
+							<input type="radio" name="priority" value="p3" />P3
+							<input type="radio" name="priority" value="p2" />P2
+							<input type="radio" name="priority" value="p1" />P1
+						</div>
 					</div>
 				</div>
-				<div className="post-input">
-					<input type="text" placeholder="请输入TodoItem"/>
-				</div>
-				<div className="post-filter">
-					<p>选择优先级</p>
-					<input type="radio" name="priority" value="p1"/>P1
-					<input type="radio" name="priority" value="p2" />P2
-					<input type="radio" name="priority" value="p3" />P3
-				</div>
-				<div className="post-confirm">
-					添加任务
-				</div>
-	
-			</div>
-		)
+			);
+		} else {
+			return null;
+		}
 	}
 }
 
--- React/app/components/container.jsx
+++ React/app/components/container.jsx
@@ -4,14 +4,28 @@ import Post from './Post/post.jsx';
 import './container.scss';
 
 class Container extends Component {
+	constructor(props) {
+		super(props);
+		this.handlePostShow = this.handlePostShow.bind(this);
+		this.state = {
+			postShow: false
+		};
+	}
+
+	handlePostShow(condition) {
+		this.setState({
+			postShow: condition
+		});
+	}
+
 	render() {
+		let postShow = this.state.postShow;
 		return (
 			<div className="main-container"> 
-				<Header />
-				<Post />
+				<Header handlePostShow={this.handlePostShow} />
+				<Post handlePostShow={this.handlePostShow} show={postShow}/>
 			</div>
 		);
 	}
 }
-
 export default Container
