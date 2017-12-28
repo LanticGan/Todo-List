fff10d2bb478c782ecc6aa6ce8d8f3acc35bb441
lantic <gytlantic@outlook.com>
2017-12-28 15:27:22 +0800

    Add .ignore

--- Django/TodoList/todo/static/app/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import Container from './components/container.jsx';
-
-ReactDom.render(
-	<Container />,
-	document.querySelector("#root")
-);
--- Django/TodoList/todo/urls.py
+++
@@ -1,6 +0,0 @@
-from todo import views
-from django.conf.urls import include, url
-urlpatterns = [
-    url(r'^list/$', views.todo_list),
-    url(r'^item/(?P<pk>[0-9]+)/$', views.todo_item_detail),
-]
