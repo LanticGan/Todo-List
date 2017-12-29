d1521eccd2364ee287fa8b12d20e8b4c2dda70f3
lantic <gytlantic@outlook.com>
2017-12-30 00:19:19 +0800

    Optimize code style

--- React/app/components/DoingItems/doingitems.js
+++ React/app/components/DoingItems/doingitems.js
@@ -7,47 +7,41 @@ class DoingItems extends React.Component {
 
 	constructor(props) {
 		super(props);
-
-		this.deleteItem = this.deleteItem.bind(this);
-		this.completeItem = this.completeItem.bind(this);
-		this.editItem = this.editItem.bind(this);
-		this.showEdit = this.showEdit.bind(this);
-		this.submitNewItem = this.submitNewItem.bind(this);
-
 		this.state = {
 			editingItemIndex: -1,
 			editingItem: ''
 		}
 	}
 
-	deleteItem(e) {
+	deleteItem = (e) => {
 		let itemId = e.target.name;
 		this.props.store.deleteItem(0, itemId);
 	}
 
-	completeItem(e) {
+	completeItem = (e) => {
 		let itemId = Number(e.target.getAttribute('name'));
 		this.props.store.completeItem(itemId);
 	}
 
-	editItem(e) {
+	editItem = (e) => {
 		this.setState({
 			editingItem: e.target.value
 		})
 	}
 
-	submitNewItem(e) {
+	submitNewItem = (e) => {
 		e.preventDefault();
-		let [itemIndex, newItemContent] = [Number(e.target.getAttribute('itemindex')), e.target.newitem.value];
-
+		let [itemIndex, newItemContent] = [
+			Number(e.target.getAttribute('itemindex')), 
+			e.target.newitem.value
+		];
 		this.setState({
 			editingItemIndex: -1
 		})
-		
 		this.props.store.editItem(itemIndex, newItemContent);
 	}
 
-	showEdit(e) {
+	showEdit = (e) => {
 		let text = e.target.textContent;
 		let itemID = e.target.getAttribute('itemID');
 		this.setState({
@@ -57,46 +51,46 @@ class DoingItems extends React.Component {
 	}
 
 	render() {
-		let { store } = this.props;
+		const { store } = this.props;
 		let itemsContent = store.doingItems,
 			items = [];
 		let isEditing = this.state.isEditing,
 			editingItemIndex = Number(this.state.editingItemIndex);
 		if (itemsContent.length) {
 			items = itemsContent.map((item, index) => {
-			let itemPrioriyClass = `priority-${item.priority}`
-			return (
-				<div className="item" key={index}>
-					<div  className={`${itemPrioriyClass} done-box`} name={index} onClick={this.completeItem}>
+				let itemPrioriyClass = `priority-${item.priority}`
+				return (
+					<div className="item" key={index}>
+						<div  className={`${itemPrioriyClass} done-box`} name={index} onClick={this.completeItem}>
+						</div>
+						{
+							(editingItemIndex != index) &&
+							<div className="item-content" onClick={this.showEdit} itemID={index}>
+								{item.content}
+							</div>
+						}	
+						{
+							(editingItemIndex == index) &&
+							<div className="edit-item">
+								<form onSubmit={this.submitNewItem} itemindex={index}>
+										<input className="item-input" type="text" name="newitem" 
+											value={this.state.editingItem} onChange={this.editItem} 
+											autoFocus
+										/>
+										<input type="submit" style={{display:'none'}} />
+								</form>
+							</div>
+						}
+							<div className="expire-date">
+								{item.expireDate.format("MM/DD/YYYY")}
+							</div>
+							<div className="cancel" onClick={this.deleteItem}>
+								<img src="assets/cancel.png" name={index} />
+							</div>
 					</div>
-					{
-						(editingItemIndex != index) &&
-						<div className="item-content" onClick={this.showEdit} itemID={index}>
-							{item.content}
-						</div>
-					}	
-					{
-						(editingItemIndex == index) &&
-						<div className="edit-item">
-							<form onSubmit={this.submitNewItem} itemindex={index}>
-									<input className="item-input" type="text" name="newitem" 
-										value={this.state.editingItem} onChange={this.editItem} 
-										autoFocus
-									/>
-									<input type="submit" style={{display:'none'}} />
-							</form>
-						</div>
-					}
-						<div className="expire-date">
-							{item.expireDate.format("MM/DD/YYYY")}
-						</div>
-						<div className="cancel" onClick={this.deleteItem}>
-							<img src="assets/cancel.png" name={index} />
-						</div>
-				</div>);
+				);
 			});
 		}
-
 		return (
 			<div className="doing-items">
 				{items}
@@ -105,4 +99,4 @@ class DoingItems extends React.Component {
 	}
 }
 
-export default DoingItems
+export default DoingItems;
